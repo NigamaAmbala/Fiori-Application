75752b8001999e6c186bb2b92e9ec0a811d01ac9
Nigama Ambala <nigama.ambala@artihcus.com>
2024-04-22 09:09:09 +0000

    Aggregation binding

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -10,27 +10,7 @@ sap.ui.define([
 
         return Controller.extend("com.app.fioriapplication.controller.Home", {
             onInit: function() {
-                const oPerson = {
-                    employee: {
-                        EmployeeName: "Ryka",
-                        Age:20,
-                        Salary:"10lpa"
-
-                    }
-                }
-                const oPerson1 = {
-                    employee: {
-                        EmployeeName: "Nigama",
-                        Age:20,
-                        Salary:"12lpa"
-
-                    }
-                }
-
-                const oJsonModel = new JSONModel(oPerson);
-                const localMode1 = new JSONModel(oPerson1);
-                this.getView().setModel(oJsonModel);
-                this.getView().setModel(localMode1,"Employee1");
+                
             }
 
         });
